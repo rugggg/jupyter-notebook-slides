98db0487486532b70efa49e45caf347ddedb2b4d
Doug Woodward <woodw116@mail.chapman.edu>
2016-12-06 17:42:08 -0800

    Update

--- publish.sh
+++ publish.sh
@@ -10,6 +10,6 @@ git checkout -B gh-pages
 cp -r /tmp/workspace/* .
 git add -A .
 git commit -m "new version"
-git push origin master gh-pages
+git push -f origin master gh-pages
 git checkout master
 rm -rf /tmp/workspace
